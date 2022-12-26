a489fb79aa3eac82d8decb8c687be334401a09f4
pechenegam <ivan.m.2002@yandex.by>
2022-12-26 17:41:51 +0100

    migration correction

--- src/main/resources/db/migration/V2__insert.sql
+++ src/main/resources/db/migration/V2__insert.sql
@@ -18,16 +18,3 @@ VALUES (2, 2);
 
 INSERT INTO usr_role (user_id, role_id)
 VALUES (1, 1);
-
---exchange_rate
---insert into exchange_rate(id, created_date, updated_date, currency, ecb_date, rate)
---values (1, '2020-01-01','2020-01-01', 'USD', '2022-12-20', 1.0622);
---
---insert into exchange_rate (id, created_date, updated_date, currency, ecb_date, rate)
---values (2, '2020-01-01','2020-01-01', 'PLN', '2022-12-20', 4.6423);
---
---insert into conversion(id, created_date, updated_date, ecb_date, from_value, to_value, conversion_from_id, conversion_to_id, author_id)
---values (1, '2020-01-01','2020-01-01', '2022-12-20', 1, 0.229, 2, 1, 1);
---
---insert into conversion(id, created_date, updated_date, ecb_date, from_value, to_value, conversion_from_id, conversion_to_id, author_id)
---values (2, '2020-01-01','2020-01-01', '2022-12-20', 1, 0.229, 2, 1, 2);
